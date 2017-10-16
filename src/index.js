89d2e8b53340cfa4df46730e02fa694392177dd0
Colin T.A. Gray <colinta@gmail.com>
2017-10-16 17:18:18 -0600

    trying to use redux-persist v5

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux'
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { createLogger } from 'redux-logger'
+import { persistStore } from 'redux-persist-immutable'
+import { persistReducer, PersistGate } from 'redux-persist/lib'
+import storage from 'redux-persist/lib/storage'
 import './index.css';
 import App from './App';
 import AuthContainer from './components/AuthContainer';
@@ -23,18 +26,26 @@ logConfig.stateTransformer = (state) => {
 const logger = createLogger(logConfig)
 
 const sagaMiddleWare = createSagaMiddleware()
-
-const store = createStore(reducers, applyMiddleware(sagaMiddleWare, logger))
+const config = {
+  key: 'root',
+  storage,
+}
+const appReducer = persistReducer(config, reducers)
+const store = createStore(appReducer, applyMiddleware(sagaMiddleWare, logger))
+let persistor = persistStore(store)
 
 sagaMiddleWare.run(sagas)
 
 const root = document.getElementById('root')
 const app = (
-  <Provider store={store}>
-    <AuthContainer>
-      <App />
-    </AuthContainer>
-  </Provider>
+    <PersistGate
+      persistor={persistor}
+      loading={<em>Loading</em>}
+      >
+      <AuthContainer>
+        <App />
+      </AuthContainer>
+    </PersistGate>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
