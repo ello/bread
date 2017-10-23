0172a40a9b47d958beb97d8881bc4366a47c6b7d
Alan Peabody <gapeabody@gmail.com>
2017-10-23 13:31:30 -0400

    Add basename support.

--- src/env.js
+++ src/env.js
@@ -4,10 +4,12 @@ if (envEl) {
   module.exports = {
     OAUTH_CLIENT_ID: webappEnv.OAUTH_CLIENT_ID,
     APP_DEBUG:       webappEnv.APP_DEBUG,
+    BASENAME:        webappEnv.BASENAME,
   }
 } else {
   module.exports = {
     OAUTH_CLIENT_ID: process.env.REACT_APP_OAUTH_CLIENT_ID,
     APP_DEBUG:       process.env.REACT_APP_DEBUG,
+    BASENAME:        process.env.REACT_APP_BASENAME,
   }
 }
