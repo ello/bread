a974f27dc6acc3038b27fcf6f2c2123a1072d6dd
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-10-31 15:24:46 -0600

    Add hover state for SubmissionCount chart
    - Still needs to be styled

--- src/components/ArtistInviteDashboard/SubmissionCount.js
+++ src/components/ArtistInviteDashboard/SubmissionCount.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { VictoryLabel, VictoryLegend, VictoryPie } from 'victory'
+import {
+  VictoryLabel,
+  VictoryLegend,
+  VictoryPie,
+  VictoryTooltip,
+} from 'victory'
 
 export default class SubmissionCount extends Component {
   static propTypes = {
@@ -17,14 +22,44 @@ export default class SubmissionCount extends Component {
     totalSelectedSubmissions: null,
   }
 
-  render() {
+  unapprovedSubmissions = () => {
+    const { totalUnapprovedSubmissions } = this.props
+    const total = totalUnapprovedSubmissions.toJS()
+    return Object.assign(total, { label: `Unapproved\n${total.submissions}` })
+  }
+
+  declinedSubmissions = () => {
+    const { totalDeclinedSubmissions } = this.props
+    const total = totalDeclinedSubmissions.toJS()
+    return Object.assign(total, { label: `Declined\n${total.submissions}` })
+  }
+
+  approvedSubmissions = () => {
+    const { totalApprovedSubmissions } = this.props
+    const total = totalApprovedSubmissions.toJS()
+    return Object.assign(total, { label: `Approved\n${total.submissions}` })
+  }
+
+  selectedSubmissions = () => {
+    const { totalSelectedSubmissions } = this.props
+    const total = totalSelectedSubmissions.toJS()
+    return Object.assign(total, { label: `Selected\n${total.submissions}` })
+  }
+
+  totalSubmissionCount = () => {
     const {
-      totalApprovedSubmissions,
       totalUnapprovedSubmissions,
       totalDeclinedSubmissions,
+      totalApprovedSubmissions,
       totalSelectedSubmissions,
     } = this.props
+    return totalDeclinedSubmissions.get('submissions', 0)
+      + totalUnapprovedSubmissions.get('submissions', 0)
+      + totalApprovedSubmissions.get('submissions', 0)
+      + totalSelectedSubmissions.get('submissions', 0)
+  }
 
+  render() {
     return (
       <div style={{width: "300px", height: "300px"}}>
         <svg viewBox="0 0 400 400">
@@ -33,14 +68,14 @@ export default class SubmissionCount extends Component {
             standalone={false}
             padding={60}
             data={[
-              totalUnapprovedSubmissions.toJS(),
-              totalDeclinedSubmissions.toJS(),
-              totalApprovedSubmissions.toJS(),
-              totalSelectedSubmissions.toJS(),
+              this.unapprovedSubmissions(),
+              this.declinedSubmissions(),
+              this.approvedSubmissions(),
+              this.selectedSubmissions(),
             ]}
             x="submissions"
             y="status"
-            labels={(data) => ""}
+            labelComponent={<VictoryTooltip/>}
             colorScale={["lightgray", "red", "green", "orange"]}
           />
           <VictoryLabel
@@ -49,7 +84,7 @@ export default class SubmissionCount extends Component {
             style={{ fontSize: 48, fontWeight: 600 }}
             x={200}
             y={200}
-            text={165}
+            text={this.totalSubmissionCount()}
           />
           <VictoryLegend
             x={25}
