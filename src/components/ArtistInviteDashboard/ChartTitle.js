787cec2e18e2c587cf3fbe6db54465e98ddb01d7
Grant Blakeman <hello@grantblakeman.com>
2017-11-10 10:29:04 -0800

    chart titles - remove unused

--- src/components/ArtistInviteDashboard/ChartTitle.js
+++ src/components/ArtistInviteDashboard/ChartTitle.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 import styled from 'styled-components'
 import { ff, fs } from '../../constants/styled/font_stack'
-import { colors } from '../../constants/styled/colors'
 
 const ChartTitleHolder = styled.h4`
   ${ff.light.full}
