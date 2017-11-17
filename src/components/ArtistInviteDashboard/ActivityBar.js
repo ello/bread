683e174bb96c4d6f2412ea1f640204a29798f35b
Alan Peabody <gapeabody@gmail.com>
2017-11-17 17:06:13 -0500

    Fix ActivityBar rendering by adding a loader.
    Two issues here really, both solved by the same fix - showing a loader
    until the data is fully loaded.
    
    1. Victory was throwing an error when trying to render w/ no data. This
    happens the first time you visit any artist invite.
    
    2. The activity is pretty slow to load, especially for invite where
    @ello is the brand user.

--- src/components/ArtistInviteDashboard/ActivityBar.js
+++ src/components/ArtistInviteDashboard/ActivityBar.js
@@ -57,55 +57,73 @@ export default class ActivityBar extends Component {
     return word.charAt(0).toUpperCase() + word.slice(1)
   }
 
+  isLoaded = () => {
+    const {
+      totalCommentActivity,
+      totalFollowerActivity,
+      totalLoveActivity,
+      totalMentionActivity,
+      totalRepostActivity,
+    } = this.props
+    return !totalCommentActivity.isEmpty() &&
+      !totalFollowerActivity.isEmpty() &&
+      !totalLoveActivity.isEmpty() &&
+      !totalMentionActivity.isEmpty() &&
+      !totalRepostActivity.isEmpty()
+  }
+
   render() {
     return (
       <div className="chart-container full">
         <ChartTitle title="On Network Activity" />
-        <div className="chart-structure">
-          <svg className="chart bar">
-            <VictoryChart
-              standalone={false}
-              width={1360}
-              height={310}
-              padding={{ top: 40, bottom: 40, left: 0, right: 0 }}
-            >
-              <VictoryAxis
-                dependentAxis={true}
-                tickValues={[0,1,2,3,4,5]}
-                tickLabelComponent={ <VictoryLabel dx="-5" verticalAnchor="end" textAnchor="end" lineHeight="1.75" /> }
-                style={{
-                  axis: {stroke: "translucent"},
-                  axisLabel: {fontSize: 20, fontFamily: typeface.regular, padding: 30},
-                  grid: {stroke: colors.mediumGrey},
-                  tickLabels: {fontSize: 12, fontFamily: typeface.regular, fill: colors.mediumGrey, padding: -34},
-                }}
-              />
-              <VictoryAxis
-                offsetY={40}
-                domain={{ x: [0, 6] }}
-                tickFormat={(t) => this.capitalize(t)}
-                style={{
-                  axis: {stroke: "translucent"},
-                  tickLabels: {fontSize: 12, fontFamily: typeface.regular, fill: colors.black},
-                }}
-              />
-              <VictoryBar
+        {this.isLoaded() ?
+          <div className="chart-structure">
+            <svg className="chart bar">
+              <VictoryChart
                 standalone={false}
-                labelComponent={<VictoryTooltip pointerLength={8} pointerWidth={14} cornerRadius={0} height={50} width={100} orientation='top' flyoutStyle={{fill: colors.black}} style={{fill: colors.white, fontSize: 12, fontFamily: typeface.regular}} />}
-                data={[
-                  this.commentActivity(),
-                  this.followerActivity(),
-                  this.loveActivity(),
-                  this.mentionActivity(),
-                  this.repostActivity(),
-                ]}
-                style={{ data: { width: 40 } }}
-                x="type"
-                y="activities"
-              />
-            </VictoryChart>
-          </svg>
-        </div>
+                width={1360}
+                height={310}
+                padding={{ top: 40, bottom: 40, left: 0, right: 0 }}
+              >
+                <VictoryAxis
+                  dependentAxis={true}
+                  tickValues={[0,1,2,3,4,5]}
+                  tickLabelComponent={ <VictoryLabel dx="-5" verticalAnchor="end" textAnchor="end" lineHeight="1.75" /> }
+                  style={{
+                    axis: {stroke: "translucent"},
+                    axisLabel: {fontSize: 20, fontFamily: typeface.regular, padding: 30},
+                    grid: {stroke: colors.mediumGrey},
+                    tickLabels: {fontSize: 12, fontFamily: typeface.regular, fill: colors.mediumGrey, padding: -34},
+                  }}
+                />
+                <VictoryAxis
+                  offsetY={40}
+                  domain={{ x: [0, 6] }}
+                  tickFormat={(t) => this.capitalize(t)}
+                  style={{
+                    axis: {stroke: "translucent"},
+                    tickLabels: {fontSize: 12, fontFamily: typeface.regular, fill: colors.black},
+                  }}
+                />
+                <VictoryBar
+                  standalone={false}
+                  labelComponent={<VictoryTooltip pointerLength={8} pointerWidth={14} cornerRadius={0} height={50} width={100} orientation='top' flyoutStyle={{fill: colors.black}} style={{fill: colors.white, fontSize: 12, fontFamily: typeface.regular}} />}
+                  data={[
+                    this.commentActivity(),
+                    this.followerActivity(),
+                    this.loveActivity(),
+                    this.mentionActivity(),
+                    this.repostActivity(),
+                  ]}
+                  style={{ data: { width: 40 } }}
+                  x="type"
+                  y="activities"
+                />
+              </VictoryChart>
+            </svg>
+          </div> :
+          <div> Loading...</div>
+        }
       </div>
     )
   }
